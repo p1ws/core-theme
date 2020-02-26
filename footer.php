ef64e8af7b2486a5e56f6e58143ab7ff8a78384b
Jeremy Lindemann <jeremy@pageonewebsolutions.com>
2020-02-26 18:16:29 -0500

    Updated formatting in footer.php

--- footer.php
+++ footer.php
@@ -2,38 +2,36 @@
 
 					<!-- FOOTER TOP -->
 					<section id="footer--global__top" class="container--full flexrow xs--around md--between"> 
-						<!---------->
 						<!-- Logo -->
 						<a id="logo--footer" title="<?php bloginfo( 'name' ); ?>" href="<?php echo home_url(); ?>" class="xs--1 xs--align-center md--1of2 md--align-left">
 							<img src="<?php echo get_field('logo', 'options'); ?>" alt="<?php echo get_field('company_name', 'options'); ?>" />
 						</a>
-						<!------------------>
 						<!-- GLOBAL LINKS -->
 						<div id="footer--global__links" class="xs--1 xs--align-center md--1of2 md--align-right">
 							<?php include('includes/global-links.php'); ?>						
 						</div>					
 					</section>
-					
+
 					<!-- FOOTER MAIN -->
 					<section id="footer--global__main" class="container--full">
 						<nav role="navigation" class="flexrow xs--start md--between">
 							<?php wp_nav_menu(array(
-							'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-							'container_class' => 'footer-links',         // class of container (should you choose to use it)
-							'menu' => __( 'Footer Links', 'p1wscore' ),   // nav name
-							'menu_class' => 'nav footer-nav flexrow wrap xs--between',            // adding custom nav class
-							'theme_location' => 'footer-links',             // where it's located in the theme
-							'before' => '',                                 // before the menu
-							'after' => '',                                  // after the menu 
-							'link_before' => '',                            // before each link
-							'link_after' => '',                             // after each link
-							'depth' => 0,                                   // limit the depth of the nav
-							'fallback_cb' => 'p1ws_footer_links_fallback'  // fallback function
+								'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+								'container_class' => 'footer-links',         // class of container (should you choose to use it)
+								'menu' => __( 'Footer Links', 'p1wscore' ),   // nav name
+								'menu_class' => 'nav footer-nav flexrow wrap xs--between',            // adding custom nav class
+								'theme_location' => 'footer-links',             // where it's located in the theme
+								'before' => '',                                 // before the menu
+								'after' => '',                                  // after the menu 
+								'link_before' => '',                            // before each link
+								'link_after' => '',                             // after each link
+								'depth' => 0,                                   // limit the depth of the nav
+								'fallback_cb' => 'p1ws_footer_links_fallback'  // fallback function
 							)); ?>
 
 							<?php if( have_rows('global-footer_additional-content', 'options') ): ?>
 								<div id="nav__additional-content" class="xs--1 md--1of4 flexrow xs--column xs--between">															
-											
+
 									<?php while( have_rows('global-footer_additional-content', 'options') ): the_row(); ?>
 										<div class="additional-content flexrow xs--1of2 md--1 xs--start md--end">
 											<div class="xs--1 xs--align-center md--1of2 md--align-right">
@@ -44,7 +42,7 @@
 											</div>	
 										</div>		
 									<?php endwhile; ?>	
-									
+
 									<?php if( have_rows('partners', 'options') ): ?>								
 										<?php while( have_rows('partners', 'options') ): the_row(); ?>
 											<div id="partner-logos">										
@@ -54,16 +52,15 @@
 											</div>	
 										<?php endwhile; ?>			
 									<?php endif; ?>										
-									
+
 								</div>			
 							<?php endif; ?>		
- 
+
 						</nav>
 					</section>
 
 					<!-- FOOTER SUB -->
-					<section id="footer--global__sub" class="container--full flexrow xs--around md--between">					
-							<!------------>
+					<section id="footer--global__sub" class="container--full flexrow xs--around md--between">
 							<!-- SOCIAL -->
 							<?php if( have_rows('global_social','options') ): ?>
 								<div id="footer--sub__social" class="xs--1 xs--align-center">
@@ -78,7 +75,6 @@
 									</ul>
 								</div>
 							<?php endif; ?>
-							<!----------------------->
 							<!-- COPYRIGHT & LINKS -->
 							<div id="footer--sub__copyright" class="xs--1 xs--align-center md--1of2 md--align-left">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> All rights reserved.</p>
@@ -93,9 +89,8 @@
 											</li>
 										<?php endwhile; ?>
 									</ul>
-								<?php endif; ?>							
-							</div>						
-							<!---------------->
+								<?php endif; ?>
+							</div>
 							<!-- OTHER INFO -->
 							<div id="footer--sub__links" class="xs--1 xs--align-center md--1of2 md--align-right">
 
@@ -118,7 +113,7 @@
 											</p>
 										<?php endwhile; ?>
 								<?php endif; ?>							
-							</div>							
+							</div>
 					</section>
 				</footer>
 
@@ -130,7 +125,6 @@
 
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/library/slick/slick.min.js"></script>
 
-			
 	</body>
 
 	<!-- Additional Scripts -->
